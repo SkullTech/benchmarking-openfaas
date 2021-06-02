d8badb75c4e9292a55440a11d9e21b9972f6bc84
Sumit Ghosh <sumit@sumit-ghosh.com>
2021-06-02 23:41:54 +0530

    Working script

--- manage-clusters.py
+++ manage-clusters.py
@@ -2,26 +2,42 @@ import logging
 import subprocess
 import time
 from argparse import ArgumentParser
-from typing import Optional
+from pathlib import Path
+from typing import Callable, Optional
 
 import coloredlogs
+from ruamel.yaml import YAML
 
 logger = logging.getLogger()
 coloredlogs.install(level="DEBUG", logger=logger)
+yaml = YAML()
 
 
-def run_command(command: str, capture_output: bool = False) -> Optional[str]:
+def run_command(
+    command: str,
+    capture_output: bool = False,
+    success_condition: Optional[Callable[[str], bool]] = None,
+) -> Optional[str]:
     logger.debug(f"Executing: {command}")
-    cp = subprocess.run(command, shell=True, capture_output=capture_output)
+    if success_condition:
+        while True:
+            cp = subprocess.run(command, shell=True, capture_output=True)
+            if success_condition(cp.stdout.decode("UTF-8")):
+                break
+            time.sleep(10)
+    else:
+        cp = subprocess.run(command, shell=True, capture_output=capture_output)
     if capture_output:
         return cp.stdout.decode("UTF-8")
 
 
-def create_cluster(size: str, count: int) -> None:
+def create_cluster(
+    size: str, count: int, min_replicas: int, max_replicas: int, target_fips: float
+) -> None:
     logger.info(f"Creating cluster with {count} {size} nodes")
     cluster_name = f"{size}-{count}"
     run_command(
-        "doctl kubernetes cluster create --1-clicks openfaas,metrics-server "
+        "doctl kubernetes cluster create "
         f"--count {count} --region blr1 --size {size} {cluster_name}"
     )
     cluster_id = run_command(
@@ -30,6 +46,11 @@ def create_cluster(size: str, count: int) -> None:
     ).strip()
     logger.info(f"Cluster created, name: {cluster_name}, ID: {cluster_id}")
     time.sleep(10)
+
+    logging.info("Installing OpenFaaS on cluster")
+    run_command("arkade install openfaas --load-balancer")
+    logging.info("Installing metrics-server on cluster")
+    run_command("arkade install metrics-server")
 
     logger.info(f"Installing prometheus-adapter on cluster")
     run_command("helm repo update")
@@ -43,16 +64,13 @@ def create_cluster(size: str, count: int) -> None:
         "kubectl expose service prometheus -n openfaas --port=9090 --target-port=9090 "
         "--type=LoadBalancer --name=prometheus-external"
     )
-    time.sleep(180)
-    while True:
-        output = run_command(
-            "kubectl get service prometheus-external -n openfaas --no-headers",
-            capture_output=True,
-        )
-        prometheus = f"http://{output.split()[3]}:9090"
-        if "pending" not in prometheus:
-            break
-        time.sleep(10)
+
+    output = run_command(
+        "kubectl get service prometheus-external -n openfaas --no-headers",
+        capture_output=True,
+        success_condition=lambda x: "pending" not in x,
+    )
+    prometheus = f"http://{output.split()[3]}:9090"
     logger.info(f"OpenFaaS Prometheus is available at: {prometheus}")
 
     logger.info("Log into OpenFaaS CLI")
@@ -66,6 +84,7 @@ def create_cluster(size: str, count: int) -> None:
     output = run_command(
         "kubectl get services -n openfaas gateway-external --no-headers",
         capture_output=True,
+        success_condition=lambda x: "pending" not in x,
     )
     gateway = f"http://{output.split()[3]}:8080"
     logger.info(f"OpenFaaS gateway is available at: {gateway}")
@@ -73,6 +92,14 @@ def create_cluster(size: str, count: int) -> None:
 
     logger.info("Deploying function to OpenFaaS")
     run_command(f"faas-cli deploy -f function/primality.yml --gateway {gateway}")
+
+    logger.info("Deploying HPA")
+    hpa_config = yaml.load(Path("kubernetes/hpa-function-invocation-per-second.yml"))
+    hpa_config["spec"]["minReplicas"] = min_replicas
+    hpa_config["spec"]["maxReplicas"] = max_replicas
+    hpa_config["spec"]["metrics"][0]["external"]["target"]["averageValue"] = target_fips
+    yaml.dump(hpa_config, Path("kubernetes/hpa-function-invocation-per-second.yml"))
+    run_command(f"kubectl apply -f kubernetes/hpa-function-invocation-per-second.yml")
 
 
 def list_clusters():
@@ -101,6 +128,17 @@ def main():
         type=str,
         help="size of worker nodes",
         choices=["s-1vcpu-2gb", "s-2vcpu-2gb", "s-2vcpu-4gb", "s-4vcpu-8gb"],
+    )
+    create_cluster_parser.add_argument(
+        "min-replicas", type=int, help="minimum no. of replicas"
+    )
+    create_cluster_parser.add_argument(
+        "max-replicas", type=int, help="maximum no. of replicas"
+    )
+    create_cluster_parser.add_argument(
+        "target-fips",
+        type=float,
+        help="target average function invocations per second",
     )
 
     list_clusters_parser = subparsers.add_parser("list")
